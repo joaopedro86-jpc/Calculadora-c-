0a1b90400bdd6371b8736b3298a65b779416d507
Senai Desenvolvimento de sistemas <senai.dsvsis@SENAILAB.LOCAL>
2025-11-07 22:01:25 -0300

    Commit calculadora

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Exemplo.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap images {
+            get {
+                object obj = ResourceManager.GetObject("images", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap img12 {
             get {
                 object obj = ResourceManager.GetObject("img12", resourceCulture);
